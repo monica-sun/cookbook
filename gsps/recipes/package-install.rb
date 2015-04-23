6649cbfb72f132976750ba58d1c5434f699b2e57
Mayru Sun <mayru@Mayru-MacBook-Pro.local>
2015-04-24 01:44:38 +0800

    fix bug

--- gsps/recipes/package-install.rb
+++ gsps/recipes/package-install.rb
@@ -10,7 +10,12 @@ yum_package 'net-snmp' do
   action :install
 end
 
-rpm_package 'spawn-fcgi' do
-  package_name node[:rpmpath][:spawnfcgi]
-  action :install
+remote_file "#{Chef::Config[:file_cache_path]}/spawn-fcgi-1.6.3-1.el6.x86_64.rpm" do
+    source "http://s3-us-west-2.amazonaws.com/icrctestrpm/common/spawn-fcgi-1.6.3-1.el6.x86_64.rpm"
+    action :create
+end
+
+rpm_package "spwan-fcgi" do
+    source "#{Chef::Config[:file_cache_path]}/spawn-fcgi-1.6.3-1.el6.x86_64.rpm"
+    action :install
 end
