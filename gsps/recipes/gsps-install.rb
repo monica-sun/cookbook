9aff990f91940034726c5749d4a5e37404e4b1bc
Mayru Sun <mayru@Mayru-MacBook-Pro.local>
2015-04-24 08:29:08 +0800

    fix bug

--- gsps/recipes/gsps-install.rb
+++ gsps/recipes/gsps-install.rb
@@ -1,11 +1,21 @@
 node[:deploy].each do |app_name, deploy|
-  rpm_package 'spsc' do
-    package_name node[:rpmpath][:spsc]
+  remote_file "#{Chef::Config[:file_cache_path]}/spsc-2.81-1061.el5.x86_64.rpm" do
+    source "http://s3-us-west-2.amazonaws.com/monicatest/spsc-2.81-1061.el5.x86_64.rpm"
+    action :create
+  end
+
+  rpm_package "spsc" do
+    source "#{Chef::Config[:file_cache_path]}/spsc-2.81-1061.el5.x86_64.rpm"
     action :install
   end
-  
-  rpm_package 'gsps' do
-    package_name node[:rpmpath][:gsps]
+
+  remote_file "#{Chef::Config[:file_cache_path]}/gsps-2.81-1079.el6.x86_64.rpm" do
+    source "http://s3-us-west-2.amazonaws.com/monicatest/gsps-2.81-1079.el6.x86_64.rpm"
+    action :create
+  end
+
+  rpm_package "gsps" do
+    source "#{Chef::Config[:file_cache_path]}/gsps-2.81-1079.el6.x86_64.rpm"
     action :install
   end
 
--- gsps/recipes/package-install.rb
+++ gsps/recipes/package-install.rb
@@ -19,3 +19,38 @@ rpm_package "spwan-fcgi" do
     source "#{Chef::Config[:file_cache_path]}/spawn-fcgi-1.6.3-1.el6.x86_64.rpm"
     action :install
 end
+
+remote_file "#{Chef::Config[:file_cache_path]}/lighttpd-1.4.35-1.el6.x86_64.rpm" do
+    source "https://s3-us-west-2.amazonaws.com/monicatest/lighttpd-1.4.35-1.el6.x86_64.rpm"
+    action :create
+end
+
+rpm_package "lighttpd" do
+    source "#{Chef::Config[:file_cache_path]}/lighttpd-1.4.35-1.el6.x86_64.rpm"
+    action :install
+end
+
+remote_file "#{Chef::Config[:file_cache_path]}/lighttpd-fastcgi-1.4.35-1.el6.x86_64.rpm" do
+    source "http://s3-us-west-2.amazonaws.com/icrctestrpm/common/lighttpd-fastcgi-1.4.35-1.el6.x86_64.rpm"
+    action :create
+end
+
+rpm_package "lighttpd-fcgi" do
+    source "#{Chef::Config[:file_cache_path]}/lighttpd-fastcgi-1.4.35-1.el6.x86_64.rpm"
+    action :install
+end
+
+directory "/var/log/lighttpd" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+bash 'modify_sudoer' do
+  user 'root'
+  cwd '/etc'
+  code <<-EOH
+  sed -i 's/Defaults    requiretty/#Defaults    requiretty/g' /etc/sudoers
+  EOH
+end
