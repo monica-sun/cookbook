de98e30e52c1bfd448c3f1985443de1766a279be
monica-sun <monica_sun@trend.com.tw>
2015-08-13 18:33:22 +0800

    modify installed packages

--- gsps/recipes/package-install.rb
+++ gsps/recipes/package-install.rb
@@ -2,26 +2,25 @@ yum_package 'gamin' do
   action :install
 end
 
-yum_package 'fcgi' do
-  action :install
-end
+yum_package 'openssl' do
+  action :upgrade
 
 yum_package 'net-snmp' do
   action :install
 end
 
-remote_file "#{Chef::Config[:file_cache_path]}/spawn-fcgi-1.6.3-1.el6.x86_64.rpm" do
-    source "http://s3-us-west-2.amazonaws.com/icrctestrpm/common/spawn-fcgi-1.6.3-1.el6.x86_64.rpm"
+remote_file "#{Chef::Config[:file_cache_path]}/pcre-8.12-1.icrc.x86_64.rpm" do
+    source "https://s3-us-west-2.amazonaws.com/icrc-packages/common/pcre-8.12-1.icrc.x86_64.rpm"
     action :create
 end
 
-rpm_package "spwan-fcgi" do
-    source "#{Chef::Config[:file_cache_path]}/spawn-fcgi-1.6.3-1.el6.x86_64.rpm"
+rpm_package "pcre" do
+    source "#{Chef::Config[:file_cache_path]}/pcre-8.12-1.icrc.x86_64.rpm"
     action :install
 end
 
 remote_file "#{Chef::Config[:file_cache_path]}/lighttpd-1.4.35-1.el6.x86_64.rpm" do
-    source "https://s3-us-west-2.amazonaws.com/monicatest/lighttpd-1.4.35-1.el6.x86_64.rpm"
+    source "https://s3-us-west-2.amazonaws.com/icrc-packages/lighttpd-1.4.35-1.el6.x86_64.rpm"
     action :create
 end
 
@@ -30,13 +29,13 @@ rpm_package "lighttpd" do
     action :install
 end
 
-remote_file "#{Chef::Config[:file_cache_path]}/lighttpd-fastcgi-1.4.35-1.el6.x86_64.rpm" do
-    source "http://s3-us-west-2.amazonaws.com/icrctestrpm/common/lighttpd-fastcgi-1.4.35-1.el6.x86_64.rpm"
+remote_file "#{Chef::Config[:file_cache_path]}/fcgi-2.4.0-1.icrc.x86_64.rpm" do
+    source "https://s3-us-west-2.amazonaws.com/icrc-packages/fcgi-2.4.0-1.icrc.x86_64.rpm"
     action :create
 end
 
-rpm_package "lighttpd-fcgi" do
-    source "#{Chef::Config[:file_cache_path]}/lighttpd-fastcgi-1.4.35-1.el6.x86_64.rpm"
+rpm_package "fcgi" do
+    source "#{Chef::Config[:file_cache_path]}/fcgi-2.4.0-1.icrc.x86_64.rpm"
     action :install
 end
 
