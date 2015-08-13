66a2f4437253e289283338a52603a7abad47c240
monica-sun <monica_sun@trend.com.tw>
2015-08-13 18:12:22 +0800

    update spsc/gsps rpm version

--- gsps/recipes/gsps-install.rb
+++ gsps/recipes/gsps-install.rb
@@ -1,21 +1,21 @@
 node[:deploy].each do |app_name, deploy|
-  remote_file "#{Chef::Config[:file_cache_path]}/spsc-2.81-1061.el5.x86_64.rpm" do
-    source "http://s3-us-west-2.amazonaws.com/monicatest/spsc-2.81-1061.el5.x86_64.rpm"
+  remote_file "#{Chef::Config[:file_cache_path]}/spsc-2.81-1081.el5.x86_64.rpm" do
+    source "https://s3-us-west-2.amazonaws.com/icrc-packages/spsc-2.81-1081.el5.x86_64.rpm"
     action :create
   end
 
   rpm_package "spsc" do
-    source "#{Chef::Config[:file_cache_path]}/spsc-2.81-1061.el5.x86_64.rpm"
+    source "#{Chef::Config[:file_cache_path]}/spsc-2.81-1081.el5.x86_64.rpm"
     action :install
   end
 
-  remote_file "#{Chef::Config[:file_cache_path]}/gsps-2.81-1079.el6.x86_64.rpm" do
-    source "http://s3-us-west-2.amazonaws.com/monicatest/gsps-2.81-1079.el6.x86_64.rpm"
+  remote_file "#{Chef::Config[:file_cache_path]}/gsps-2.81-1103.el6.x86_64.rpm" do
+    source "https://s3-us-west-2.amazonaws.com/icrc-packages/gsps-2.81-1103.el6.x86_64.rpm"
     action :create
   end
 
   rpm_package "gsps" do
-    source "#{Chef::Config[:file_cache_path]}/gsps-2.81-1079.el6.x86_64.rpm"
+    source "#{Chef::Config[:file_cache_path]}/gsps-2.81-1103.el6.x86_64.rpm"
     action :install
   end
 
